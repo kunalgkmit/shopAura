c2614b1a1aa81ffd1812ca2248136c1f10ab89dd
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-02-23 23:50:43 +0530

    feat: implement create account api logic

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,15 +1,11 @@
 import { Asset } from 'react-native-image-picker';
 import { axiosInstance } from '../network/axiosInstance';
 import { ENDPOINTS } from '../network/URLs';
+import { SignUpProps } from '../types/auth';
 
-export const userSignUp = async () => {
-  const response = await axiosInstance.post(ENDPOINTS.AUTH.USER, {
-    name: 'Ok cool',
-    email: 'okcool123@gmail.com',
-    password: '1234',
-    avatar: 'https://api.lorem.space/image/face?w=640&h=480',
-  });
-  console.log(response);
+export const userSignUp = async (userData: SignUpProps) => {
+  const response = await axiosInstance.post(ENDPOINTS.AUTH.USER, userData);
+  return response.data;
 };
 
 export const uploadFileService = async (image: Asset) => {
