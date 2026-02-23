44bb9f14c43635e4443251b560a565174a38861c
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-02-23 15:48:57 +0530

    feat: implement sign up form with validations

--- src/components/signUpForm/SignUpForm.tsx
+++ src/components/signUpForm/SignUpForm.tsx
@@ -1,29 +1,97 @@
 import { useState } from 'react';
 
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import CustomTextInput from '../customTextInput';
 import { styles } from './styles';
+import {
+  validateConfirmPassword,
+  validateEmail,
+  validatePassword,
+  validateUserName,
+} from '../../utils/helpers';
 
 export default function SignUpForm() {
-  const [name, setName] = useState('');
+  const [fullName, setFullName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [avatar, setAvatar] = useState('');
 
   const [errors, setErrors] = useState({
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
+    fullNameError: '',
+    emailError: '',
+    passwordError: '',
+    confirmPasswordError: '',
   });
+
+  const verifyFullName = (value: string) => {
+    setFullName(value);
+    const fullNameError = validateUserName(value);
+
+    setErrors(prev => ({
+      ...prev,
+      fullNameError,
+    }));
+  };
+
+  const verifyEmail = (value: string) => {
+    setEmail(value);
+    const emailError = validateEmail(value);
+
+    setErrors(prev => ({
+      ...prev,
+      emailError,
+    }));
+  };
+
+  const verifyPassword = (pwd: string) => {
+    setPassword(pwd);
+    const passwordError = validatePassword(pwd);
+
+    setErrors(prev => ({
+      ...prev,
+      passwordError,
+    }));
+  };
+
+  const verifyConfirmPassword = (pwd: string) => {
+    setConfirmPassword(pwd);
+    const confirmPasswordError = validateConfirmPassword(pwd, password);
+
+    setErrors(prev => ({
+      ...prev,
+      confirmPasswordError,
+    }));
+  };
 
   return (
     <View style={styles.container}>
-      <CustomTextInput placeholder="Enter Name" value={name} />
-      <CustomTextInput placeholder="Enter Email" value={email} />
-      <CustomTextInput placeholder="Enter Password" value={password} />
-      <CustomTextInput placeholder="Verify Password" value={confirmPassword} />
+      <CustomTextInput
+        placeholder="Enter Full Name"
+        value={fullName}
+        onChangeText={verifyFullName}
+        error={errors.fullNameError}
+      />
+      <CustomTextInput
+        placeholder="Enter Email"
+        value={email}
+        onChangeText={verifyEmail}
+        error={errors.emailError}
+      />
+      <CustomTextInput
+        placeholder="Enter Password"
+        value={password}
+        onChangeText={verifyPassword}
+        error={errors.passwordError}
+        secureTextEntry={true}
+      />
+      <CustomTextInput
+        placeholder="Confirm Password"
+        value={confirmPassword}
+        onChangeText={verifyConfirmPassword}
+        error={errors.confirmPasswordError}
+        secureTextEntry={true}
+      />
     </View>
   );
 }
---
+++ src/utils/helpers.ts
@@ -0,0 +1,59 @@
+export const validateUserName = (userName: string) => {
+  if (!userName.trim().length) {
+    return 'Enter Full Name';
+  } else if (userName.trim().length < 3) {
+    return 'Full name must be of min. 3 characters';
+  } else if (!/^[A-Za-z]+( [A-Za-z]+)?$/.test(userName)) {
+    return 'Only letters allowed and only one space between names';
+  }
+  return '';
+};
+
+export const validateEmail = (email: string) => {
+  if (!email.trim().length) {
+    return 'Enter Email';
+  } else if (!/^[\w.+\-]+@gmail\.com$/.test(email)) {
+    return 'Enter Valid Email containing @gmail.com';
+  }
+  return '';
+};
+
+export const validatePassword = (pwd: string) => {
+    console.log('PWD>>', pwd)
+  if (!pwd) {
+    return 'Password is required';
+  }
+
+  if (pwd.length < 8) {
+    return 'Minimum 8 characters';
+  }
+
+  if (!/[A-Z]/.test(pwd)) {
+    return 'At least one uppercase letter';
+  }
+
+  if (!/[a-z]/.test(pwd)) {
+    return 'At least one lowercase letter';
+  }
+
+  if (!/\d/.test(pwd)) {
+    return 'At least one number';
+  }
+
+  if (!/[@$!%*?&]/.test(pwd)) {
+    return 'At least one special character (@$!%*?&)';
+  }
+  return '';
+};
+
+export const validateConfirmPassword = (
+  password: string,
+  confirmPassword: string,
+): string => {
+  if (!confirmPassword.length) {
+    return 'Confirm your password';
+  } else if (password !== confirmPassword) {
+    return 'Passwords do not match';
+  }
+  return '';
+};
