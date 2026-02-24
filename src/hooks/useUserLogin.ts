f684ae18e3846fc503b2aea0c0b0530c34fb8d5e
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-02-24 23:23:31 +0530

    feat: implement action dispatch on login success

--- src/hooks/useUserLogin.ts
+++ src/hooks/useUserLogin.ts
@@ -1,8 +1,12 @@
 import { useMutation } from '@tanstack/react-query';
 import { userLogin } from '@services/auth.service';
+import { store } from '@store/store';
+import { setAuthTokens } from '@store/actions/authActions';
 
 export const useUserLogin = () => {
   return useMutation({
     mutationFn: userLogin,
+    onSuccess: data =>
+      store.dispatch(setAuthTokens(data.access_token, data.refresh_token)),
   });
 };
