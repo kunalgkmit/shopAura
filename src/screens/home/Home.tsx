2daba6e2835aba4dccbc1190db0c2e4d63adc214
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-02-25 23:07:37 +0530

    feat: implement categories component in Home screen

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,13 +1,16 @@
 import { View } from 'react-native';
 import { CustomAppBar } from '@components/customAppBar';
 import Banner from '@components/banner';
+import { Categories } from '@components/categories';
+import { styles } from './styles';
 
 export default function Home() {
   return (
     <>
       <CustomAppBar title="Home" showDrawer={true} />
-      <View>
+      <View style={styles.container}>
         <Banner />
+        <Categories />
       </View>
     </>
   );
