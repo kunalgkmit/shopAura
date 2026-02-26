d1121616970f5d04b2743192964b7ad142063734
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-02-26 15:35:44 +0530

    refactor: extract and use useCallback to memoize the render function

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { FlatList, Text, View } from 'react-native';
 import { CategoryCard } from '@components/categoryCard';
 import { CATEGORIES } from '@constants/constants';
@@ -6,10 +7,18 @@ import { styles } from './styles';
 export function Categories() {
   // getCategories hook call
   // const { data: categories, isPending } = useCategories();
+
+  const renderCategory = useCallback(
+    ({ item }: { item: (typeof CATEGORIES)[0] }) => (
+      <CategoryCard categoryData={item} />
+    ),
+    [],
+  );
+
   return (
     <View style={styles.container}>
       <View style={styles.titleWrapper}>
-      <Text style={styles.title}>Categories</Text>
+        <Text style={styles.title}>Categories</Text>
       </View>
       <View style={styles.listWrapper}>
         <FlatList
@@ -17,7 +26,7 @@ export function Categories() {
           data={CATEGORIES}
           columnWrapperStyle={styles.columnWrapperStyle}
           keyExtractor={item => item.id.toString()}
-          renderItem={({ item }) => <CategoryCard categoryData={item} />}
+          renderItem={renderCategory}
           showsVerticalScrollIndicator={false}
         />
       </View>
