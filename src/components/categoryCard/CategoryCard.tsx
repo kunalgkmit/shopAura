6e9402a0bff191e6decbd0d6e195a93dc7abf780
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-02-26 12:20:48 +0530

    feat: implement pressabel category card component and back button in app bar

--- src/components/categoryCard/CategoryCard.tsx
+++ src/components/categoryCard/CategoryCard.tsx
@@ -1,11 +1,24 @@
 import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { styles } from './styles';
+import { ProductsListing } from '@screens/productsListing/ProductsListing';
+import { ROUTES } from '@constants/routes';
 
 export function CategoryCard({ categoryData }: CategoryCardProps) {
+  const navigation = useNavigation<StackNavProp>();
+
   const { id, name, image } = categoryData;
+
+  const handleCategoryPress = () => {
+    navigation.navigate(ROUTES.STACK.PRODUCT_LISTING, {
+      categoryId: id,
+      categoryName: name,
+    });
+  };
+
   return (
     <View>
-      <TouchableOpacity style={styles.box}>
+      <TouchableOpacity style={styles.box} onPress={handleCategoryPress}>
         <Image source={{ uri: image }} style={styles.categoryImage} />
         <Text style={styles.categoryName}>{name}</Text>
       </TouchableOpacity>
