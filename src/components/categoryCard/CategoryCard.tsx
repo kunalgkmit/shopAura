82a0595b2cd4915d4fa4ce406e50786b5bd5e015
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-02-26 21:22:47 +0530

    refactor: fix CategoryCard onPress issue

--- src/components/categoryCard/CategoryCard.tsx
+++ src/components/categoryCard/CategoryCard.tsx
@@ -1,15 +1,26 @@
 import { Text, TouchableOpacity, View } from 'react-native';
 import Ionicons from '@react-native-vector-icons/ionicons';
+import { useNavigation } from '@react-navigation/native';
 import { COLORS } from '@constants/colors';
-import { styles } from './styles';
-import { ProductsListing } from '@screens/productsListing/ProductsListing';
 import { ROUTES } from '@constants/routes';
+import { styles } from './styles';
 
 export function CategoryCard({ categoryData }: CategoryCardProps) {
   const { name, icon, color } = categoryData;
+
+  const navigation = useNavigation<StackNavProp>();
+
+  const handleCategoryPress = () => {
+    navigation.navigate(ROUTES.STACK.PRODUCT_LISTING, {
+      categoryName: name,
+    });
+  };
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={[styles.box, { backgroundColor: color }]}>
+      <TouchableOpacity
+        style={[styles.box, { backgroundColor: color }]}
+        onPress={handleCategoryPress}
+      >
         <View style={styles.iconWrapper}>
           <Ionicons name={icon} size={30} color={COLORS.BG_CARD} />
         </View>
