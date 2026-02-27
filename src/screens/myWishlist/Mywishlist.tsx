a21e72567923748f2bc9727dd0ad9ae30c589888
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-02-27 16:02:43 +0530

    feat: final app

--- src/screens/myWishlist/Mywishlist.tsx
+++ src/screens/myWishlist/Mywishlist.tsx
@@ -1,4 +1,4 @@
-import { FlatList, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import { useSelector } from 'react-redux';
 import { CustomAppBar } from '@components/customAppBar';
 import { WishlistCard } from '@components/wishlistCard/WishlistCard';
@@ -11,6 +11,7 @@ export default function Mywishlist() {
       <CustomAppBar title="My Wishlist" showDrawer />
       <View style={styles.container}>
         <FlatList
+          ListEmptyComponent={<Text>Wishlist is Empty</Text>}
           data={wishlistItems}
           renderItem={({ item }) => <WishlistCard productListingData={item} />}
         />
